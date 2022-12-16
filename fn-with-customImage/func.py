9dadd4f5bd5c3a52bb1298504886776f03f24cae
Mahendra Mehra <mahendra488@gmail.com>
2022-12-16 22:02:23 +0530

    Update func.py

--- fn-with-customImage/func.py
+++ fn-with-customImage/func.py
@@ -13,6 +13,6 @@ def handler(ctx, data: io.BytesIO=None):
     logging.getLogger().info("Invoked function with custom image") 
     return response.Response(
         ctx, 
-        response_data=json.dumps({"status": "Hello! You have succesfully deployed a python application to OCI Functions using OCI DevOps CI/CD Pipeline"}),
+        response_data=json.dumps({"status": "Hello! You have succesfully deployed a python application to OCI Functions using OCI DevOps CI/CD Pipeline v1"}),
         headers={"Content-Type": "application/json"}
     )
